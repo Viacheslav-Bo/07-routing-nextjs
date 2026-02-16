f682e867cabae903a5b66c5973c39ca7fd69f629
Viacheslav-Bo <zotikivna@gmail.com>
2026-02-17 01:00:07 +0200

    hot fix

--- app/@modal/ModalBack.client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import RouteModal from '@/components/Modal/Modal';
-
-export default function ModalBack({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  return <RouteModal onClose={() => router.back()}>{children}</RouteModal>;
-}
