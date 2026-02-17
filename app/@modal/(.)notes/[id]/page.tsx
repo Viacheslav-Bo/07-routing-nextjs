9522bc93b1fb1b4a4a0a2477cd9721f1b0cf485f
Viacheslav-Bo <zotikivna@gmail.com>
2026-02-17 16:39:37 +0200

    fix

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,11 @@
+import NotePreviewClient from './NotePreview.client';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return <NotePreviewClient id={id} />;
+}
